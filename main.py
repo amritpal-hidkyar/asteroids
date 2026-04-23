fa50aec65f26fbd59670d67d5ca39411bc001b5f
amritpal-hidkyar <amrit.hidkyar@gmail.com>
2026-04-23 07:46:21 -0400

    added groups and asteroid.py file

--- main.py
+++ main.py
@@ -1,17 +1,40 @@
 import pygame 
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        updatable.update(dt)
+
+        for object in drawable:
+            object.draw(screen)
+        
         screen.fill("black")
+
+        for object in drawable:
+            object.draw(screen)
+            
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
